46133cac7cfcd3eb2468ff8a70e4aed58a649bfc
WordRocket <122843310+WordRocket@users.noreply.github.com>
2026-06-17 16:51:03 -0400

    Updated package-lock.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,14 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
+import icon from 'astro-icon';
 import siteConfig from './src/data/site-config.json';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
   site: siteConfig.url,
-  integrations: [sitemap()],
+  integrations: [sitemap(), icon()],
   vite: {
     plugins: [tailwindcss()],
   },
